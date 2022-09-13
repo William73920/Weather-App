6a321f3e0f2095cb67fc92359399580ef4d8ed5b
Rohit Rozario <rrozario958@gmail.com>
2022-09-13 12:44:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express=require("express");
+const https=require("https");
+const bodyParser=require("body-parser");
+
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html")
+    
+    
+})
+app.post("/",function(req,res){
+    const query=req.body.cityName;
+    const unit = "metric";
+    const appid="1f5f315fc33bfd66ff386a922be076a7";
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+query +"&appid="+appid+"&units="+unit;
+
+    https.get(url,function(response){
+        console.log(response.statusCode);
+
+        response.on("data",function(data){
+            const weatherData=JSON.parse(data);
+            console.log(weatherData);
+            // JSON.stringify(data);
+            const temp=weatherData.main.temp;
+            const name=weatherData.name;
+            const description=weatherData.weather[0].description;
+            const icon=weatherData.weather[0].icon;
+            const imageURL="http://openweathermap.org/img/wn/"+icon+"@2x.png"
+            console.log(temp,description);
+            res.write("<p>The weather is "+ description + "<p/>");
+            res.write("<h1>The temperature in " +name+" is "+temp+" degrees celcius.</h1>");
+            res.write("<img src="+imageURL+">");
+            res.send();
+        })
+
+    })
+    
+})
+
+
+
+
+
+
+
+app.listen("3000",function(){
+    console.log("server started on port 3000");
+
+})
